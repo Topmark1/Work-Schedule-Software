ab19574764ddf2a903c7dfc3e41b8dc3c52e1854
Temitope Akande <topsiology11@gmail.com>
2023-04-16 14:54:24 +0100

    week button logic

--- app/page.js
+++ app/page.js
@@ -6,6 +6,9 @@ function HomePage() {
   //worker name state, local host can be used or a backend
 const [workerName,setWorkerName]  = useState({wN:"",name:["Worker 1","Worker 2","Worker 3","Worker 4","Worker 5","Worker 6","Worker 7","Worker 8"]})
 
+const [weekDesign,rearrangeDesign] = useState([])
+
+
  // for hard coding name when available
   let n1 = workerName.name[0]
   let n2 = workerName.name[1]
@@ -18,37 +21,39 @@ const [workerName,setWorkerName]  = useState({wN:"",name:["Worker 1","Worker 2",
   let workersNams = [n1,n2,n3,n4,n5,n6,n7,n8]
   
   //eight possible matrix combination to ensure equl schedule among staff in 8 weeks span
-  let week1 = [1,2,3,4,5,6,7,8]
-  let week2 = [8,1,2,3,4,5,6,7]
-  let week3 = [7,8,1,2,3,4,5,6]
-  let week4 = [6,7,8,1,2,3,4,5]
-  let week5 = [5,6,7,8,1,2,3,4]
-  let week6 = [4,5,6,7,8,1,2,3]
-  let week7 = [3,4,5,6,7,8,1,2]
-  let week8 = [2,3,4,5,6,7,8,1]
+  let weekArrangements = [
+                 [1,2,3,4,5,6,7,8],
+                 [8,1,2,3,4,5,6,7],
+                 [7,8,1,2,3,4,5,6],
+                 [6,7,8,1,2,3,4,5],
+                 [5,6,7,8,1,2,3,4],
+                 [4,5,6,7,8,1,2,3],
+                 [3,4,5,6,7,8,1,2],
+                 [2,3,4,5,6,7,8,1],]
+
+    /*   days.map((day)=>
+        <td className="border px-4 pb-[1px]"><span><span className="text-[10px]">{getInitials(worker)}</span><span className="flex">{timing.map((time)=>(<span className="text-[9px] whitespace-nowrap bg-green-300 py-1 pr-1">{time}</span>))}</span></span></td>)
+                */
+  let design1 = (<span>1</span>)//Monday to sunday of this week
+  let design2 = (<span>2</span>)
+  let design3 = (<span>3</span>)
+  let design4 = (<span>4</span>)
+  let design5 = (<span>5</span>)
+  let design6 = (<span>6</span>)
+  let design7 = (<span>7</span>)
+  let design8 = (<span>8</span>)
+      // weekDesign = [8,1,2,3,4,5,6,7]
+  let design =  [design1,design2,design3,design4,design5,design6,design7,design8,]
+  let designArranged = []
+  for(let i=0;i<design.length;i++){ 
+    designArranged[i] = design[weekDesign[i]-1]
+  }
   
 
-  function workSchedule(w1,w2,w3,w4,w5,w6,w7,w8,week){
-      week = week1 //depending on param supply at wee button click so should be declared inside the button oitside this function      
-      let schedule  = {
-      w1:{name:w1,sche:[]},
-      w2:{name:w2,sche:[]},
-      w3:{name:w3,sche:[]},
-      w4:{name:w4,sche:[]},
-      w5:{name:w5,sche:[]},
-      w6:{name:w6,sche:[]},
-      w7:{name:w7,sche:[]},
-      w8:{name:w8,sche:[]},
-    }
-
-
-
-
-    return schedule
-  }
-console.log(workSchedule(n1,n2,n3,n4,n5,n6,n7,n8))
-
-
+  function thisDesign(weekNUM){
+    let weekArrangement = weekDesign;
+    weekArrangement = weekArrangements[weekNUM-1];
+      rearrangeDesign(weekArrangement) }
 
 let timing = ["07:00","07:30","08:00","8:30","09:00","09:30","10:00","10:30","11:00","11:30","12:00","12:30","13:00","13:30","14:00","14:30","15:00","15:30","16:00","16:30","17:00","17:30","18:00","18:30","19:00","19:30","20:00","20:30","21:00","21:30","22:00","22:30","23:00","22:30- Night Shit -07:30",]
 let weeks = [1,2,3,4,5,6,7,8]
@@ -59,7 +64,7 @@ function getInitials(name) {
 }
   return (
     <div className="px-2 pt-3 sm:px-10 sm:pt-10">
-        <div className="flex flex-wrap items-center ">
+        <div className="flex flex-wrap items-center text-center">
             <h1 className="text-4xl font-CD font-black mb-4">Workers  Schedule</h1>
             <div className="flex flex-col items-center lg:items-end  text-center">
 
@@ -97,10 +102,14 @@ function getInitials(name) {
 
         </div>
               {/* Weeks buttons */}
-              <div className="flex flex-wrap lg:space-x-2 ml-5 mb-2 text-center">
+              <div className="flex flex-wrap lg:space-x-2 sm:ml-5 mb-2 text-center">
                 {
                   weeks.map((week)=>(
-                    <button className="px-5 py-2.5 mx-1 lg:mx-0 rounded hover:bg-[#AAC4E6] active:bg-[#4786d9] hover:shadow-2xl bg-white focus:bg-[#4786d9]">Week {week}</button> 
+                    <button 
+                    className="px-5 py-2.5 mx-1 lg:mx-0 rounded hover:bg-[#AAC4E6] active:bg-[#004DB3] hover:shadow-2xl bg-white focus:bg-[#004DB3] focus:text-[#AAC4E6]"
+                    key={week} 
+                    onClick={()=>thisDesign(week)}
+                    >Week {week}</button> 
                   ))
                 }
               </div>
@@ -120,9 +129,7 @@ function getInitials(name) {
               {workersNams.map((worker)=>
             (<tr className="hover:bg-gray-100 border-2 border-gray-300">
                 <td className="border px-4 py-1 whitespace-nowrap">{worker}</td>
-                { days.map((day)=>
-                <td className="border px-4 pb-[1px]"><span><span className="text-[10px]">{getInitials(worker)}</span><span className="flex">{timing.map((time)=>(<span className="text-[9px] whitespace-nowrap bg-green-300 py-1 pr-1">{time}</span>))}</span></span></td>)
-                }
+                {designArranged[workersNams.indexOf(worker)]}
               </tr> )
               )}
             </tbody>
